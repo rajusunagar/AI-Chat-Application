a8a4566c8344bee0cbbe75f896fef3461eb1a2b3
rajusunagar <rajusunagarcs@gmail.com>
2025-02-11 23:21:59 +0530

    Initial commit

---
+++ src/test/java/com/deepseek/InsightGPT/InsightGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepseek.InsightGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
